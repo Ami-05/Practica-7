4d0a8b7227996afb9ee3a9810382bc5c9c86ea8e
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-04-02 08:26:51 -0600

    Calculadora 2.c

--- Calculadora 2.c
+++ Calculadora 2.c
@@ -1,102 +1,121 @@
-
+//Cruz Apodaca Amiel Merari//
+//En este programa le hicimos algunas adecuaciones al programa pasado de nuestra calculadora, le sumamos la funcion de raiz cuadrada y de exponencial ademas incluimos todo en un while//
 //Inicio//
 #include <stdio.h>
 #include <math.h>
-
+//Inicio//
 int main()
 {
     //Inicializamos variables//
     int res = 0;
     int a = 0;
     int b = 0;
-    int c = 0;
     int op;
- 
- //Imprimimos Menu//
-    printf ("escoje una opción; \n");
-    printf ("1- suma \n");
-    printf ("2- resta \n");
-    printf ("3- multiplicación \n");
-    printf ("4- división\n");
-    printf ("5- raiz\n");
-    printf ("6- división\n");
-    printf ("0- potencia\n");
-  //Leer op//  
-    scanf ("%u" , &op);
-  // Si op es diferente de 0//
-    while (op !=0)
+
+    //Imprimir Menu//
+    do
+    { 
+        printf ("Menu: \n");
+        printf ("escoje una opción; \n");
+        printf ("1- Suma \n");
+        printf ("2- Resta \n");
+        printf ("3- Multiplicación \n");
+        printf ("4- División\n");
+        printf ("5- Raiz cuadrada\n");
+        printf ("6- Potencia\n");
+        printf ("0- Salida\n");
+        //Leer op//  
+        scanf ("%i" , &op);
+    }
+  // Si op < 0 y op > 6//
+    while (op < 0 || op > 6);
+do
 {
     
    
     //ingresar a la opción escogida//
-    switch (op==1);
+    switch (op)
     {
-        scanf ("%d" ,& a);
-        scanf ("%d" ,& b);
-        printf ("ingresa el primer valor;\n");
-        printf ("ingrese el segundo valor;\n");
-        res=a+b;
-    }
-    
-    {
-        switch (op==2);
-        {
-            scanf ("%d" ,& a);
-            scanf ("%d" ,& b);
+        case 1: 
+            
             printf ("ingresa el primer valor;\n");
+            scanf ("%i" ,& a);
             printf ("ingrese el segundo valor;\n");
+            scanf ("%i" ,& b);
+            res=a+b;
+            break;
+        
+        
+        case 2:
+            printf ("ingresa el primer valor;\n");
+            scanf ("%i" ,& a);
+            printf ("ingrese el segundo valor;\n");
+            scanf ("%i" ,& b);
             res=a-b;
-        }
+            break;
+        
     
-        switch (op==3);
-        {
-            scanf ("%d" ,& a);
-            scanf ("%d" ,& b);
+        case 3:
+        
             printf ("ingresa el primer valor;\n");
+            scanf ("%i" ,& a);
             printf ("ingrese el segundo valor;\n");
+            scanf ("%i" ,& b);
             res=a*b;
-        }
+            break;
+        
     
-        switch (op==4);
-        {
-            scanf ("%d" ,& a);
-            scanf ("%d" ,& b);
+        case 4:
+        
             printf ("ingresa el primer valor;\n");
+            scanf ("%i" ,& a);
             printf ("ingrese el segundo valor;\n");
+            scanf ("%i" ,& b);
             res=a/b;
-        }
+            break;
         
-        switch (op==5);
-        {
-            printf ("Si eligio la opcion 5 ingrese un unico valor;\n");
-            scanf ("%d" ,& c);
-            res= sqrt(c);
-        }
         
-        switch (op==6);
-        { 
-            scanf ("%d" ,& a);
-            scanf ("%d" ,& b);
-            scanf ("%d" ,& a);
-            scanf ("%d" ,& b);
+        case 5:
+        
+            printf ("Ingrese valor;\n");
+            scanf ("%i" ,& a);
+              if (a>0) {
+              res= sqrt(a);}
+              break;
+        
+        
+        case 6:
+         
+            printf ("ingresa la base del numero;\n");
+            scanf ("%i" ,& a);
+            printf ("ingresa la potencia de la base;\n");
+            scanf ("%i" ,& b);
             res= pow(a,b);
-        }
+            break;
+        
     }
     
 //Imprimir res//
-printf ("la operación es;%d\n", res);
+printf ("el reultado de la operación es;%i\n", res);
+printf ("\n\n");
+
 //Imprimir Menú//
-    printf ("escoje una opción; \n");
-    printf ("1- suma \n");
-    printf ("2- resta \n");
-    printf ("3- multiplicación \n");
-    printf ("4- división\n");
-    printf ("5- raiz\n");
-    printf ("6- división\n");
-    printf ("0- potencia\n");
+
+     printf ("Menu: \n");
+        printf ("escoje una opción; \n");
+        printf ("1- Suma \n");
+        printf ("2- Resta \n");
+        printf ("3- Multiplicación \n");
+        printf ("4- División\n");
+        printf ("5- Raiz cuadrada\n");
+        printf ("6- Potencia\n");
+        printf ("0- Salida\n");
     //Leer op//
-    scanf ("%u" , &op);
+    scanf ("%i" , &op);
 }
+    //Si op es diferente de 0//
+    while (op != 0);
 
 return 0;
 }
+//Fin//
